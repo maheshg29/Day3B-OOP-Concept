8611dcd0731f8c817de83852c8160e756bf338cb
mahesh <gangurdemahesh9@gmail.com>
2023-03-17 21:47:27 +0530

    [mahesh]add program file for UC3

---
+++ Day3B-OOP-Concepts/AccessModifier.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3B_OOP_Concepts
+{
+    public class AccessModifier
+    {
+        public int bankAccountNumber = 123456;
+        public void GeneralInformation()
+        {
+            Console.WriteLine("This is Public class Access Modifier");
+        }
+    }
+
+    public class BankAccount
+    {
+        private int _balance=5000;
+
+        public int GetBalance()
+        {
+            return _balance;
+        }
+
+        public void Deposit(int amount)
+        {
+            _balance += amount;
+        }
+    }
+
+    public class Shape
+    {
+        protected int Width { get; set; }
+        protected int Height { get; set; }
+
+        public virtual int GetArea()
+        {
+            return Width * Height;
+        }
+    }
+    public class Rectangle : Shape
+    {
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
--- Day3B-OOP-Concepts/Program.cs
+++ Day3B-OOP-Concepts/Program.cs
@@ -11,6 +11,8 @@ namespace Day3B_OOP_Concepts
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Day 3B Practice Problem ");
+            
+            //Type of Constructor//
             TypeOfConstructor typeOfConstructor = new TypeOfConstructor();
 
             TypeOfConstructor parameterConstructor = new TypeOfConstructor("Mahesh", 30);
@@ -23,9 +25,21 @@ namespace Day3B_OOP_Concepts
             ExampleOfPrivetClass.employeeName = "Gangurde";
             Console.WriteLine(ExampleOfPrivetClass.DisplayDetails());
 
+            //Interface//
             BankManager bankManager=new BankManager();
             bankManager.GetBankDetails();
-  
+
+            //Access Modifier//
+            AccessModifier accessModifier=new AccessModifier();
+            Console.WriteLine("We can access bank account number variable due to Public access modifier\n Bank Account number is "+accessModifier.bankAccountNumber);
+            BankAccount bankAccount=new BankAccount();
+            Console.WriteLine("This is Example of Private Access modifier \n like now we can not get balance directly but \n we can use this by public method with same class "+ bankAccount.GetBalance());
+
+            Rectangle rectangle = new Rectangle(25, 60);
+            
+            int area = rectangle.GetArea();
+
+            Console.WriteLine("Area of the given value is = "+area);
         }
      }
 }

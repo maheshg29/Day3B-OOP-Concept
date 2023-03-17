609bf6caa2c892ad163c5fe93d8e79a6cbf044dd
mahesh <gangurdemahesh9@gmail.com>
2023-03-17 21:05:05 +0530

    [mahesh]add program file for UC2

---
+++ Day3B-OOP-Concepts/Interface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3B_OOP_Concepts
+{
+    //public class Interface
+    //{
+       
+    //}
+    interface IBankDetails
+    {
+        void GetBankDetails();
+    }
+
+    class BankManager: IBankDetails
+    {
+        public void GetBankDetails()
+        {
+            Console.WriteLine("This is Bank of India");
+        }
+    }
+}
--- Day3B-OOP-Concepts/Program.cs
+++ Day3B-OOP-Concepts/Program.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace Day3B_OOP_Concepts
 {
      class Program
-    {
+     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Day 3B Practice Problem ");
             TypeOfConstructor typeOfConstructor = new TypeOfConstructor();
-            
-            TypeOfConstructor parameterConstructor = new TypeOfConstructor("Mahesh",30);
-            
+
+            TypeOfConstructor parameterConstructor = new TypeOfConstructor("Mahesh", 30);
+
             TypeOfConstructor copyConstructor = new TypeOfConstructor(parameterConstructor);
 
             TypeOfConstructor.SampleMessage();
@@ -22,7 +22,10 @@ namespace Day3B_OOP_Concepts
             //ExampleOfPrivetClass exampleOfPrivetClass = new ExampleOfPrivetClass(); // we can not create instance of this class due to Private Constructor
             ExampleOfPrivetClass.employeeName = "Gangurde";
             Console.WriteLine(ExampleOfPrivetClass.DisplayDetails());
+
+            BankManager bankManager=new BankManager();
+            bankManager.GetBankDetails();
   
         }
-    }
+     }
 }

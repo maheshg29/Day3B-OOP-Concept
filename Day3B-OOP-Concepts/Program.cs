cf0cc0e5f07022c335cca12ac8d2993572b1c6d4
mahesh <gangurdemahesh9@gmail.com>
2023-03-17 20:50:46 +0530

    [mahesh]add program file for Different type of Constructor

--- Day3B-OOP-Concepts/Program.cs
+++ Day3B-OOP-Concepts/Program.cs
@@ -11,6 +11,18 @@ namespace Day3B_OOP_Concepts
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Day 3B Practice Problem ");
+            TypeOfConstructor typeOfConstructor = new TypeOfConstructor();
+            
+            TypeOfConstructor parameterConstructor = new TypeOfConstructor("Mahesh",30);
+            
+            TypeOfConstructor copyConstructor = new TypeOfConstructor(parameterConstructor);
+
+            TypeOfConstructor.SampleMessage();
+
+            //ExampleOfPrivetClass exampleOfPrivetClass = new ExampleOfPrivetClass(); // we can not create instance of this class due to Private Constructor
+            ExampleOfPrivetClass.employeeName = "Gangurde";
+            Console.WriteLine(ExampleOfPrivetClass.DisplayDetails());
+  
         }
     }
 }
---
+++ Day3B-OOP-Concepts/TypeOfConstructor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3B_OOP_Concepts
+{
+    public class TypeOfConstructor
+    {
+        //Default Constructor
+        public TypeOfConstructor()
+        {
+            Console.WriteLine("Default Constructor Active");
+        }
+
+        //Parameterized Constructor
+        string userName;
+        int userAge;
+        public TypeOfConstructor(string name, int age)
+        {
+            userName = name;
+            userAge = age;
+            Console.WriteLine("Parameterized Constructor Active and Parameter given is \n User Name" + name + " User Age " + age);
+        }
+
+        //Copy Constructor 
+        public TypeOfConstructor(TypeOfConstructor typeOfConstructor)
+        {
+            Console.WriteLine("Copy Constructor is Active");
+        }
+
+        //Static Constructor
+        static TypeOfConstructor()
+        {
+            Console.WriteLine("Static Constructor Active");
+        }
+        public static void SampleMessage()
+        {
+            Console.WriteLine("This is Sample Message for Static class activation");
+        }
+
+        //
+    }
+
+    public class ExampleOfPrivetClass
+    {
+        private ExampleOfPrivetClass()
+        {
+            Console.WriteLine("Privet Class active ");
+            
+        }
+        public static string employeeName;
+        public static string DisplayDetails()
+        {
+            return employeeName;
+        }
+    }
+       
+}
